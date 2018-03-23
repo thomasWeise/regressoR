6efe7c23755bd5b8b5aadcb09b764db9b4b70d10
Thomas Weise <tweise@hfuu.edu.cn>
2018-03-23 15:39:13 +0800

    Yet More Reduction

--- tests/testthat/test_learn.R
+++ tests/testthat/test_learn.R
@@ -7,7 +7,7 @@ regressoR.learn.choice <- as.integer(runif(n=1, min=1, max=3))
 test_that("Test regressoR.learn I", {
   if(slow.tests || (regressoR.learn.choice == 1L)) {
     if(slow.tests) { n <- 100L; }
-    else           { n <- 20L;  }
+    else           { n <- 5L;  }
     dx <- rnorm(n);
     dy <- rnorm(n=n, mean=50*dx*dx-33);
     result <- regressoR.learn(x=dx, y=dy);
@@ -20,7 +20,7 @@ test_that("Test regressoR.learn I", {
 test_that("Test regressoR.learn II", {
   if(slow.tests || (regressoR.learn.choice == 2L)) {
     if(slow.tests) { n <- 100L; }
-    else           { n <- 20L;  }
+    else           { n <- 5L;  }
     dx <- runif(n) + 0.1
     dy.raw <- 1 + 0.4* exp(5 - 3*dx + 0.6*dx*dx)
     dy <- rnorm(n=n, mean=dy.raw)
