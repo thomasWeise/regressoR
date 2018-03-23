02d145d763a38a592c4ad3d96ad5b9fb89b13e0e
Thomas Weise <tweise@hfuu.edu.cn>
2018-03-23 14:30:22 +0800

    Further Reduction of Tests

--- tests/testthat/test_learn.R
+++ tests/testthat/test_learn.R
@@ -1,27 +1,30 @@
 library("regressoR")
 context("regressoR.learn")
 
-need.fast.tests <- (!(is.na(Sys.getenv("TRAVIS", unset=NA))));
+slow.tests <- (is.na(Sys.getenv("TRAVIS", unset=NA)))
+regressoR.learn.choice <- as.integer(runif(n=1, min=1, max=3))
 
 test_that("Test regressoR.learn I", {
-  if(need.fast.tests) { n <- 40; }
-  else                { n <- 100; }
-  dx <- rnorm(n);
-  dy <- rnorm(n=n, mean=50*dx*dx-33);
-  result <- regressoR.learn(x=dx, y=dy);
-  expect_true(!(is.null(result)));
-  expect_is(result, "FittedModel");
-  validObject(result);
+  if(slow.tests || (regressoR.learn.choice == 1L)) {
+    n  <- 100;
+    dx <- rnorm(n);
+    dy <- rnorm(n=n, mean=50*dx*dx-33);
+    result <- regressoR.learn(x=dx, y=dy);
+    expect_true(!(is.null(result)));
+    expect_is(result, "FittedModel");
+    validObject(result);
+  }
 })
 
 test_that("Test regressoR.learn II", {
-  if(need.fast.tests) { n <- 40; }
-  else                { n <- 100; }
-  dx <- runif(n) + 0.1
-  dy.raw <- 1 + 0.4* exp(5 - 3*dx + 0.6*dx*dx)
-  dy <- rnorm(n=n, mean=dy.raw)
-  result <- regressoR.learn(x=dx, y=dy);
-  expect_true(!(is.null(result)));
-  expect_is(result, "FittedModel");
-  validObject(result);
+  if(slow.tests || (regressoR.learn.choice == 2L)) {
+    n  <- 100;
+    dx <- runif(n) + 0.1
+    dy.raw <- 1 + 0.4* exp(5 - 3*dx + 0.6*dx*dx)
+    dy <- rnorm(n=n, mean=dy.raw)
+    result <- regressoR.learn(x=dx, y=dy);
+    expect_true(!(is.null(result)));
+    expect_is(result, "FittedModel");
+    validObject(result);
+  }
 })
