c5e1966e09ed1e317f532208d4ca4e3d4f203338
Thomas Weise <tweise@hfuu.edu.cn>
2018-03-23 15:19:02 +0800

    Further Reduction of Tests

--- tests/testthat/test_learn.R
+++ tests/testthat/test_learn.R
@@ -6,7 +6,8 @@ regressoR.learn.choice <- as.integer(runif(n=1, min=1, max=3))
 
 test_that("Test regressoR.learn I", {
   if(slow.tests || (regressoR.learn.choice == 1L)) {
-    n  <- 100;
+    if(slow.tests) { n <- 100L; }
+    else           { n <- 20L;  }
     dx <- rnorm(n);
     dy <- rnorm(n=n, mean=50*dx*dx-33);
     result <- regressoR.learn(x=dx, y=dy);
@@ -18,7 +19,8 @@ test_that("Test regressoR.learn I", {
 
 test_that("Test regressoR.learn II", {
   if(slow.tests || (regressoR.learn.choice == 2L)) {
-    n  <- 100;
+    if(slow.tests) { n <- 100L; }
+    else           { n <- 20L;  }
     dx <- runif(n) + 0.1
     dy.raw <- 1 + 0.4* exp(5 - 3*dx + 0.6*dx*dx)
     dy <- rnorm(n=n, mean=dy.raw)
