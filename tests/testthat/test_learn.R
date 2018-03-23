0ae1efc13fb81803ee5f9882d9cef502b222509f
Thomas Weise <tweise@hfuu.edu.cn>
2018-03-23 14:02:07 +0800

    Trying to Speed-Up Tests for Travis-CI

--- tests/testthat/test_learn.R
+++ tests/testthat/test_learn.R
@@ -1,9 +1,13 @@
 library("regressoR")
 context("regressoR.learn")
 
+need.fast.tests <- (!(is.na(Sys.getenv("TRAVIS", unset=NA))));
+
 test_that("Test regressoR.learn I", {
-  dx <- rnorm(100);
-  dy <- rnorm(n=100, mean=50*dx*dx-33);
+  if(need.fast.tests) { n <- 40; }
+  else                { n <- 100; }
+  dx <- rnorm(n);
+  dy <- rnorm(n=n, mean=50*dx*dx-33);
   result <- regressoR.learn(x=dx, y=dy);
   expect_true(!(is.null(result)));
   expect_is(result, "FittedModel");
@@ -11,9 +15,11 @@ test_that("Test regressoR.learn I", {
 })
 
 test_that("Test regressoR.learn II", {
-  dx <- runif(100) + 0.1
+  if(need.fast.tests) { n <- 40; }
+  else                { n <- 100; }
+  dx <- runif(n) + 0.1
   dy.raw <- 1 + 0.4* exp(5 - 3*dx + 0.6*dx*dx)
-  dy <- rnorm(n=100, mean=dy.raw)
+  dy <- rnorm(n=n, mean=dy.raw)
   result <- regressoR.learn(x=dx, y=dy);
   expect_true(!(is.null(result)));
   expect_is(result, "FittedModel");
