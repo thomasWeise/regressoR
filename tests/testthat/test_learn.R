c70338caec9d379c5851a19dfa7f8ed9a706521a
Thomas Weise <tweise@hfuu.edu.cn>
2018-03-26 11:15:50 +0800

    Now Using Newest Version of regressoR.functional.models
    ...we cannot test on the fitting anymore,
    because that now is too slow.

--- tests/testthat/test_learn.R
+++ tests/testthat/test_learn.R
@@ -2,12 +2,10 @@ library("regressoR")
 context("regressoR.learn")
 
 slow.tests <- (is.na(Sys.getenv("TRAVIS", unset=NA)))
-regressoR.learn.choice <- as.integer(runif(n=1, min=1, max=3))
 
 test_that("Test regressoR.learn I", {
-  if(slow.tests || (regressoR.learn.choice == 1L)) {
-    if(slow.tests) { n <- 100L; }
-    else           { n <- 5L;  }
+  if(slow.tests) {
+    n <- 100L;
     dx <- rnorm(n);
     dy <- rnorm(n=n, mean=50*dx*dx-33);
     result <- regressoR.learn(x=dx, y=dy);
@@ -18,9 +16,8 @@ test_that("Test regressoR.learn I", {
 })
 
 test_that("Test regressoR.learn II", {
-  if(slow.tests || (regressoR.learn.choice == 2L)) {
-    if(slow.tests) { n <- 100L; }
-    else           { n <- 5L;  }
+  if(slow.tests) {
+    n <- 100L;
     dx <- runif(n) + 0.1
     dy.raw <- 1 + 0.4* exp(5 - 3*dx + 0.6*dx*dx)
     dy <- rnorm(n=n, mean=dy.raw)
