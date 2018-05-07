1956bebf4dbde62eac4ad170cc6fb191e818085f
Thomas Weise <tweise@hfuu.edu.cn>
2018-05-08 07:51:34 +0800

    Now using plotteR as BackEnd for Plotting

--- R/batchPlot.R
+++ R/batchPlot.R
@@ -4,27 +4,15 @@
 #' @description A simple utility method for visualizing the data.
 #' @param results an instance or list of \code{\link{RegressionResult}}
 #' @param log the logarithmic scaling info for the axes
-#' @importFrom grDevices rgb
-#' @importFrom stats runif
-#' @export regressoR.batchPlot
-regressoR.batchPlot <- function(results, log="") {
-  x.min <- +Inf;
-  y.min <- +Inf;
-  x.max <- -Inf;
-  y.max <- -Inf;
-
-  for(result in results) {
-    x.min <- min(x.min, min(result@metric@x));
-    x.max <- max(x.max, max(result@metric@x));
-    y.min <- min(y.min, min(result@metric@y));
-    y.max <- max(y.max, max(result@metric@y));
-  }
-
-  plot(x=c(x.min, x.max), y=c(y.min, y.max), log=log, type="n", xlab="", ylab="");
-  for(result in results) {
-    col <- rgb(runif(1),runif(1), runif(1));
-    points(x=result@metric@x, y=result@metric@y, col=col, lwd=0.5);
-    srt <- sort(result@metric@x);
-    lines(x=srt, y=result@result@f(srt), col=col, lwd=1.5);
-  }
+#' @param plotPoints should the points be plotted?
+#' @param plotFun should the fitted function models be plotted?
+#' @importFrom plotteR batchPlot.list
+#' @export batchPlot.RegressionResults
+batchPlot.RegressionResults <- function(results, log="", plotPoints=TRUE, plotFun=TRUE) {
+  batchPlot.list(data=results,
+                 xfun=function(result) result@metric@x,
+                 yfun=function(result) result@metric@y,
+                 ffun=function(result, x) result@result@f(x),
+                 plotXY=plotPoints,
+                 plotXF=plotFun)
 }
