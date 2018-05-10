930006f416ce770d4c0ee4289f565b1124f2fce1
Thomas Weise <tweise@hfuu.edu.cn>
2018-05-10 16:50:01 +0800

    Added Example (which is running very strangely slow?)

---
+++ examples/fitAndPlot.R
@@ -0,0 +1,55 @@
+set.seed(20140208L)
+
+library(plotteR)
+library(regressoR)
+
+# make an example
+make.example <- function(f) {
+  x <- sort(runif(n=21)); # generate x data
+  y <- rnorm(n=length(x),
+             mean=f(x),
+             s=0.075);  # noisy y
+  return(list(x=x, y=y, f=f));
+}
+
+# the three base functions
+f1 <- function(x) 1 - 0.2*x + x*x
+f2 <- function(x) 0.1 * exp(3 - x)
+f3 <- function(x) 1.2 + 0.7*sin(5*x)
+
+f1.ex <- make.example(f1);
+f2.ex <- make.example(f2);
+f3.ex <- make.example(f3);
+
+old.par <- par(mfrow=c(3, 2))
+
+batchPlot.list(list(f1.ex, f2.ex, f3.ex),
+               xfun=function(l) l$x,
+               yfun=function(l) l$y,
+               ffun=function(l,x) l$f(x),
+               names=c("f1", "f2", "f3"),
+               main="Original Data")
+
+for(q in 0:4/4) {
+  cat("Now learning f1 using quality q=", q, "\n", sep="", collapse="");
+  r1 <- regressoR.learnForExport(x=f1.ex$x, y=f1.ex$y, q=q);
+  cat("Now learning f2 using quality q=", q, "\n", sep="", collapse="");
+  r2 <- regressoR.learnForExport(x=f2.ex$x, y=f2.ex$y, q=q);
+  cat("Now learning f3 using quality q=", q, "\n", sep="", collapse="");
+  r3 <- regressoR.learnForExport(x=f3.ex$x, y=f3.ex$y, q=q);
+  cat("Done learning, now plotting.\n");
+  X <- list(r1, r2, r3);
+  batchPlot.RegressionResults(X, plotXY=TRUE, plotXF=TRUE,
+                              main=paste("q=", q, sep="", collapse=""),
+                              names=vapply(X,
+                                           FUN=function(r)
+                                             paste(r@name, "(",
+                                                   round(r@time, digits=0),
+                                                   ", ",
+                                                   round(r@result@quality,
+                                                         digits=2), ")",
+                                                   sep="", collapse=""),
+                                           FUN.VALUE=""));
+}
+
+par(old.par)
