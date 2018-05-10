fff52182254309d6507b5684f7f813961b2d8835
Thomas Weise <tweise@hfuu.edu.cn>
2018-05-10 16:56:04 +0800

    More Comments in Oddly Slow Example

--- examples/fitAndPlot.R
+++ examples/fitAndPlot.R
@@ -17,19 +17,24 @@ f1 <- function(x) 1 - 0.2*x + x*x
 f2 <- function(x) 0.1 * exp(3 - x)
 f3 <- function(x) 1.2 + 0.7*sin(5*x)
 
+# create the three example data sets
 f1.ex <- make.example(f1);
 f2.ex <- make.example(f2);
 f3.ex <- make.example(f3);
 
-old.par <- par(mfrow=c(3, 2))
+# we want to put 6 figues next to each other: the original data/function and
+# fitting results at five quality levels
+old.par <- par(mfrow=c(3, 2));
 
+# plot the original data
 batchPlot.list(list(f1.ex, f2.ex, f3.ex),
                xfun=function(l) l$x,
                yfun=function(l) l$y,
                ffun=function(l,x) l$f(x),
                names=c("f1", "f2", "f3"),
-               main="Original Data")
+               main="Original Data and Function Values for x")
 
+# Automatically learn models and plot the results
 for(q in 0:4/4) {
   cat("Now learning f1 using quality q=", q, "\n", sep="", collapse="");
   r1 <- regressoR.learnForExport(x=f1.ex$x, y=f1.ex$y, q=q);
@@ -39,17 +44,15 @@ for(q in 0:4/4) {
   r3 <- regressoR.learnForExport(x=f3.ex$x, y=f3.ex$y, q=q);
   cat("Done learning, now plotting.\n");
   X <- list(r1, r2, r3);
+
+  # plot the regression results
   batchPlot.RegressionResults(X, plotXY=TRUE, plotXF=TRUE,
                               main=paste("q=", q, sep="", collapse=""),
+                              # as names, use the fitting quality
                               names=vapply(X,
-                                           FUN=function(r)
-                                             paste(r@name, "(",
-                                                   round(r@time, digits=0),
-                                                   ", ",
-                                                   round(r@result@quality,
-                                                         digits=2), ")",
-                                                   sep="", collapse=""),
+                                           FUN=function(r) round(r@result@quality),
                                            FUN.VALUE=""));
 }
 
-par(old.par)
+# restore old settings
+invisible(par(old.par))
