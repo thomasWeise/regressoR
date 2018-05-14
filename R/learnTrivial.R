dd1ea465d24184f0a9f9d1befcd4c4d5e3906055
Thomas Weise <tweise@hfuu.edu.cn>
2018-05-15 04:44:55 +0800

    Now Returning FittedFunctionalModels where trivially possible

--- R/learnTrivial.R
+++ R/learnTrivial.R
@@ -4,8 +4,11 @@
 # @param yy the \code{y} coordinates, i.e., the output values
 # @return an instance of \code{\link{FittedModel}} which represents the
 #   relationship between the \code{x} and \code{y} values
-#' @importFrom regressoR.functional.models FunctionalModel.linear.from.two.points FunctionalModel.quadratic.from.three.points
-#' @importFrom regressoR.base FittedModel.new
+#' @importFrom regressoR.functional.models
+#'   FunctionalModel.linear.from.two.points
+#'   FunctionalModel.quadratic.from.three.points FunctionalModel.quadratic
+#'   FunctionalModel.linear FunctionalModel.constant
+#' @importFrom regressoR.functional FittedFunctionalModel.new
 #' @importFrom functionComposeR function.canonicalize
 .regressoR.learnTrivial <- function(xx, yy) {
   # find all unique points
@@ -17,38 +20,43 @@
 
   if(n <= 1L) {
     # if there is only a single point, return a constant function
-    a <- uniPoints[[1L]][2L];
-    f <- function.canonicalize(function(x) a);
-    return(regressoR.base::FittedModel.new(f=f, q=0, size=1L));
-  }
-
-  if(n <= 2L) {
-    # if there are two points, we can connect them as linear function
-    v <- FunctionalModel.linear.from.two.points(uniPoints[[1L]][1L],
-                                                uniPoints[[1L]][2L],
-                                                uniPoints[[2L]][1L],
-                                                uniPoints[[2L]][2L]);
-    if(!(is.null(v))) {
-      a <- v[1];
-      b <- v[2];
-      f <- function.canonicalize(function(x) a+b*x);
-      return(FittedModel.new(f=f, q=0, size=2L));
+    a <- uniPoints[[1L]][2L]; a <- force(a);
+    if(is.finite(a)) {
+      return(FittedFunctionalModel.finalize(
+             FittedFunctionalModel.new(model=FunctionalModel.constant(),
+                                       par=c(a),
+                                       quality=0)));
     }
   } else {
-    # if there are three points, we can try to connect them as quadratic function
-    if(n <= 3L) {
-      v <- FunctionalModel.quadratic.from.three.points(uniPoints[[1L]][1L],
-                                                       uniPoints[[1L]][2L],
-                                                       uniPoints[[2L]][1L],
-                                                       uniPoints[[2L]][2L],
-                                                       uniPoints[[3L]][1L],
-                                                       uniPoints[[3L]][2L]);
-      if(!(is.null(v))) {
-        a <- v[1];
-        b <- v[2];
-        c <- v[3];
-        f <- function.canonicalize(function(x) a+b*x+c*x*x);
-        return(FittedModel.new(f=f, q=0, size=3L));
+    if(n <= 2L) {
+      # if there are two points, we can connect them as linear function
+      par <- FunctionalModel.linear.from.two.points(uniPoints[[1L]][1L],
+                                                    uniPoints[[1L]][2L],
+                                                    uniPoints[[2L]][1L],
+                                                    uniPoints[[2L]][2L]);
+      if(!(is.null(par))) {
+        par <- force(par);
+        return(FittedFunctionalModel.finalize(
+               FittedFunctionalModel.new(model=FunctionalModel.linear(),
+                                         par=par,
+                                         quality=0)));
+      }
+    } else {
+      # if there are three points, we can try to connect them as quadratic function
+      if(n <= 3L) {
+        par <- FunctionalModel.quadratic.from.three.points(uniPoints[[1L]][1L],
+                                                           uniPoints[[1L]][2L],
+                                                           uniPoints[[2L]][1L],
+                                                           uniPoints[[2L]][2L],
+                                                           uniPoints[[3L]][1L],
+                                                           uniPoints[[3L]][2L]);
+        if(!(is.null(par))) {
+          par <- force(par);
+          return(FittedFunctionalModel.finalize(
+                 FittedFunctionalModel.new(model=FunctionalModel.quadratic(),
+                                           par=par,
+                                           quality=0)));
+        }
       }
     }
   }
