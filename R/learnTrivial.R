e6df4d7e7d798e0cfd06810c551a9cfe3f51c3ed
Thomas Weise <tweise@hfuu.edu.cn>
2018-05-23 12:12:09 +0800

    Adapted to Trivial Solutions

--- R/learnTrivial.R
+++ R/learnTrivial.R
@@ -5,7 +5,6 @@
 # @return an instance of \code{\link{FittedModel}} which represents the
 #   relationship between the \code{x} and \code{y} values
 #' @importFrom regressoR.functional.models
-#'   FunctionalModel.linear.from.two.points FunctionalModel.linear
 #'   FunctionalModel.constant
 #' @importFrom regressoR.functional FittedFunctionalModel.new
 #'   FittedFunctionalModel.finalize
@@ -29,7 +28,6 @@
     }
   }
 
-  # there are more than three unique points (or three points that do not form a
-  # quadratic function), so we cannot have a trivial solution
+  # if we get here, then we have no trivial solution
   return(NULL);
 }
